3f95688e20659dcb873ae63822b7868612d03dbc
MartinPC <MartinChrisPC@hotmail.com>
2024-04-23 16:57:47 -0500

    ADD product route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import userRouter from './user'
 import categoryRouter from './category'
+import productRouter from './product'
 
 const router: Router = Router()
 
 router.use('/users', userRouter)
 router.use('/category', categoryRouter)
+router.use('/product', productRouter)
 
 export default router
