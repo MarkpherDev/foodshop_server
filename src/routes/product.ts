02d8529660e731eef8fb0f51f783d3d41d5146a4
MartinPC <MartinChrisPC@hotmail.com>
2024-04-23 21:42:30 -0500

    ADD Product CRUD route

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -4,6 +4,10 @@ import { ProductController } from '../controllers/product.controller'
 
 const router: Router = Router()
 
+router.get('/', ProductController.getProducts)
+router.get('/:id', ProductController.getProductById)
 router.post('/', upload.single('image'), ProductController.createProduct)
+router.put('/:id', upload.single('image'), ProductController.updateProduct)
+router.delete('/:id', ProductController.deleteProduct)
 
 export default router
