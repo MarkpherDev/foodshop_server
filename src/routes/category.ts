bca5f9940ff60af9f38526b56775e3b95b013c23
MartinPC <MartinChrisPC@hotmail.com>
2024-04-25 21:09:27 -0500

    ADD auth middleware

--- src/routes/category.ts
+++ src/routes/category.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import { CategoryController } from '../controllers/category.controller'
+import { authenticate } from '../middleware/isLogged'
 
 const router: Router = Router()
 
 router.get('/', CategoryController.getCategories)
-router.post('/', CategoryController.createCategory)
-router.put('/:id', CategoryController.updateCategory)
 router.get('/:id', CategoryController.getCategoryById)
-router.delete('/:id', CategoryController.deleteCategory)
+router.post('/', authenticate, CategoryController.createCategory)
+router.put('/:id', authenticate, CategoryController.updateCategory)
+router.delete('/:id', authenticate, CategoryController.deleteCategory)
 
 export default router
--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,13 +1,24 @@
 import { Router } from 'express'
 import upload from '../middleware/handleMulter'
 import { ProductController } from '../controllers/product.controller'
+import { authenticate } from '../middleware/isLogged'
 
 const router: Router = Router()
 
 router.get('/', ProductController.getProducts)
 router.get('/:id', ProductController.getProductById)
-router.post('/', upload.single('image'), ProductController.createProduct)
-router.put('/:id', upload.single('image'), ProductController.updateProduct)
-router.delete('/:id', ProductController.deleteProduct)
+router.post(
+	'/',
+	authenticate,
+	upload.single('image'),
+	ProductController.createProduct
+)
+router.put(
+	'/:id',
+	authenticate,
+	upload.single('image'),
+	ProductController.updateProduct
+)
+router.delete('/:id', authenticate, ProductController.deleteProduct)
 
 export default router
