255eac6c8f47aaf096207ceb6b8db167589461b7
MartinPC <MartinChrisPC@hotmail.com>
2024-04-29 01:10:33 -0500

    ADD Types for User Routes

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,66 +1,17 @@
-import { Category, Pedido, Product, User } from '@prisma/client'
+import { User } from '@prisma/client'
 
-/** Auth */
-export type UserId = User['id']
-
-export type UserData = Pick<
-	User,
-	'first_name' | 'last_name' | 'email' | 'password'
->
+/** User Schema */
+export type UserBody = Pick<User, 'name' | 'lastname' | 'email' | 'password'>
 
 export type UserResponse = Pick<
 	User,
-	'id' | 'first_name' | 'last_name' | 'email' | 'typeUser'
+	'id' | 'name' | 'lastname' | 'email' | 'type'
 >
 
-export type Payload = Pick<
-	User,
-	'id' | 'first_name' | 'last_name' | 'email' | 'typeUser'
->
-
-export type AuthResponse = {
-	user: UserResponse
+export type LoginResponse = UserResponse & {
 	token: string
 }
 
 export type AuthBody = Pick<User, 'email' | 'password'>
 
-/** Category */
-
-export type CategoryBody = Pick<Category, 'name' | 'description'>
-
-export type CategoryResponse = Pick<Category, 'id' | 'name' | 'description'>
-
-export type CategoryId = Category['id']
-
-export type CategoryUpdateBody = {
-	name?: Category['name']
-	description?: Category['description']
-}
-
-/** Product */
-
-export type ProductBody = Pick<
-	Product,
-	'name' | 'description' | 'precio' | 'categoryId' | 'status'
->
-
-export type ProductResponse = Pick<
-	Product,
-	'name' | 'description' | 'precio' | 'image' | 'status' | 'categoryId'
->
-
-export type CreateProductBody = ProductBody & {
-	image: Express.Multer.File
-}
-
-export type CreateProduct = ProductResponse & {
-	category: Category
-}
-
-export type ProductId = Product['id']
-
-export type UpdateProductBody = Partial<CreateProductBody>
-
-/** Pedidos */
-export type PedidoId = Pedido['id']
+export type AuthPayload = Pick<User, 'id' | 'email' | 'type'>
