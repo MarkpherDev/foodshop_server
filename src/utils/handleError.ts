6d21ee4e635a9c135acc0b99fbafc529988bb7a9
MartinPC <MartinChrisPC@hotmail.com>
2024-04-29 01:11:29 -0500

    ADD console.log for development errors

--- src/utils/handleError.ts
+++ src/utils/handleError.ts
@@ -1,10 +1,12 @@
 import { HttpException } from './HttpException'
 import { Response } from 'express'
+import { CODE } from './constants'
 
 export const errorMessage = (res: Response, error: unknown): void => {
 	if (error instanceof HttpException) {
 		res.status(error.status).json({ message: error.message })
 	} else {
-		res.status(500).json({ message: 'Hubo un error' })
+		console.log(error)
+		res.status(CODE.INTERNAL_SERVER_ERROR).json({ message: 'Hubo un error' })
 	}
 }
