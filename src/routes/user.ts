6ade043f7409ba4ca23db1b4ff61320efca454b3
MartinPC <MartinChrisPC@hotmail.com>
2024-04-29 01:09:57 -0500

    ADD express validator for user Router

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,10 +1,29 @@
 import { Router } from 'express'
+import { body } from 'express-validator'
 import { UserController } from '../controllers/user.controller'
+import { HandleInputErrors } from '../middleware/validation'
 
 const router: Router = Router()
 
-router.get('/', UserController.getUsers)
-router.post('/register', UserController.register)
-router.post('/login', UserController.login)
+router.post(
+	'/register',
+	body('name').notEmpty().withMessage('El nombre no puede ir vacío'),
+	body('lastname').notEmpty().withMessage('Los apellidos no pueden ir vacío'),
+	body('email').isEmail().withMessage('Email no válido'),
+	body('password')
+		.isLength({ min: 8 })
+		.withMessage('El password es muy corto, mínimo de 8 caracteres'),
+	HandleInputErrors,
+	UserController.register
+)
+router.post(
+	'/login',
+	body('email').isEmail().withMessage('Email no válido'),
+	body('password')
+		.isLength({ min: 8 })
+		.withMessage('El password es muy corto, mínimo de 8 caracteres'),
+	HandleInputErrors,
+	UserController.login
+)
 
 export default router
