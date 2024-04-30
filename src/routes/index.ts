de40f30ff323723722b2c49c93d2c2f95308533f
MartinPC <MartinChrisPC@hotmail.com>
2024-04-30 00:59:27 -0500

    UPDATE index router and ADD product router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
-import userRouter from './user'
+import userRouter from './user.router'
+import productRouter from './product.router'
 
 const router: Router = Router()
 
 router.use('/users', userRouter)
+router.use('/product', productRouter)
 
 export default router
