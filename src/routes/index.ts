eee56e5c18e82369284318d1e6f076edb31b2a66
MartinPC <MartinChrisPC@hotmail.com>
2024-05-03 00:12:13 -0500

    ADD order route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import userRouter from './user.router'
 import productRouter from './product.router'
+import orderRouter from './order.router'
 
 const router: Router = Router()
 
 router.use('/users', userRouter)
 router.use('/product', productRouter)
+router.use('/order', orderRouter)
 
 export default router
